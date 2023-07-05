niklata/snd-egd

commit 83926ff3865ef3b937a882514cc300ad29594bca

Corrections.
Use memmove() in the ring buffer.
Fix some sign conversion warnings.
The previous bit values were too narrow to contain a 'none',
so convert those from char to int size.

getrandom.h
@@ -15,11 +15,11 @@ struct frame_t {
 typedef struct {
     unsigned int total_out;
     int bits_out[16];
-    char prev_bits[16];
+    int prev_bits[16];
     unsigned char byte_out[16];
 #ifdef USE_AMLS
     int amls_bits_out[2][16];
-    char amls_bits[2][16];
+    int amls_bits[2][16];
     unsigned char amls_byte_out[2][16];
 #endif
 } vn_renorm_state_t;

rb.c
@@ -13,7 +13,7 @@
 #include "rb.h"
 
 /* returns 1 if store successful, otherwise 0 (error or not enough room) */
-unsigned int rb_store_byte(ring_buffer_t *rb, char b)
+unsigned int rb_store_byte(ring_buffer_t *rb, unsigned char b)
 {
     if (!rb)
         return 0;
@@ -86,7 +86,7 @@ int rb_move(ring_buffer_t *rb, char *buf, unsigned int bytes)
         /* We're filling the buffer behind the current index. */
         if (rb->index < rb->size) {
             cbytes = MIN(bytes, rb->size - rb->index);
-            memcpy(buf, rb->buf + rb->index, cbytes);
+            memmove(buf, rb->buf + rb->index, cbytes);
             bytes -= cbytes;
             rb->index += cbytes;
             rb->bytes -= cbytes;
@@ -103,7 +103,7 @@ int rb_move(ring_buffer_t *rb, char *buf, unsigned int bytes)
     } else if (rb->fill_idx > rb->index) {
         /* We're filling the buffer after the current index. */
         cbytes = MIN(bytes, rb->fill_idx - rb->index);
-        memcpy(buf, rb->buf + rb->index, cbytes);
+        memmove(buf, rb->buf + rb->index, cbytes);
         bytes -= cbytes;
         rb->index += cbytes;
         rb->bytes -= cbytes;

rb.h
@@ -15,7 +15,7 @@
 #include "string.h"
 
 typedef struct {
-    char buf[RB_SIZE];
+    unsigned char buf[RB_SIZE];
     unsigned int size; /* max size of the buffer in bytes */
     unsigned int bytes; /* current size of the buffer in bytes */
     unsigned int index;
@@ -52,7 +52,7 @@ static inline int rb_is_full(ring_buffer_t *rb)
 }
 
 /* returns 1 if store successful, otherwise 0 (error or not enough room) */
-unsigned int rb_store_byte(ring_buffer_t *rb, char b);
+unsigned int rb_store_byte(ring_buffer_t *rb, unsigned char b);
 /* returns 1 if store successful, otherwise 0 (error or not enough room) */
 unsigned int rb_store_byte_xor(ring_buffer_t *rb, unsigned char b);
 /* returns 0 on success, a negative number if not enough bytes or error */

commit febe0896912121e0bbf08510c623baec105b4436

Fix memory-size regression introduced by 83926ff38.
Upon closer reading, char was sufficiently wide since each field
here is only storing one of { 0, 1, -1 }.

getrandom.h
@@ -15,11 +15,11 @@ struct frame_t {
 typedef struct {
     unsigned int total_out;
     int bits_out[16];
-    int prev_bits[16];
+    char prev_bits[16];
     unsigned char byte_out[16];
 #ifdef USE_AMLS
     int amls_bits_out[2][16];
-    int amls_bits[2][16];
+    char amls_bits[2][16];
     unsigned char amls_byte_out[2][16];
 #endif
 } vn_renorm_state_t;